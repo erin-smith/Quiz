b51446fccbe0a0e7333e7a584e0a4a98c1e53446
Erin Smith <erin.acumen@gmail.com>
2020-10-08 16:37:56 -0700

    linting file questions.js

--- files/questions.js
+++ files/questions.js
@@ -1,51 +1,56 @@
-var questions = [
-    {
-    question: "Which of the following statements about JavaScript is FALSE?",
-    choices: ["JavaScript is the dominant client-side scripting language of the web", 
-    "Brendan Eich created it in 10 days in 1995", 
-     "JavaScript and Java are inter-related", 
-     "The first browser to run JavaScript was Netscape 2"],
-    answer: 2
-  },
- {
-    question: "Which is NOT a 'recommended practice' when using JavaScript?",
-    choices: ["Ending statements with semi-colons", "closing '{' and '(', or braces and parentheses",
-    "the use of strict mode ","Always using concat instead of + or +="],
-    answer: 3
-}, {
-    question: "Which function adds an element at the begining of an array?",
-    choices:["push()", "pop()", "shift()", "unshift()"],
-    answer: 3
-}, {
-    question: "Which of the following about 'NaN' is FALSE?",
-    choices: ["NaN is numeric", "NaN === NaN", "Number.isNaN(NaN)", "NaN is a global property"],
-    answer: 1
-}, {
-    question: "Which is TRUE about 'Math.random()' ?",
-    choices: ["It is an object", "It provides cryptographically secure random numbers", "It returns a '1' sometimes",
+const questions = [{
+  question: "Which of the following statements about JavaScript is FALSE?",
+  choices: ["JavaScript is the dominant client-side scripting language of the web",
+    "Brendan Eich created it in 10 days in 1995",
+    "JavaScript and Java are inter-related",
+    "The first browser to run JavaScript was Netscape 2"],
+  answer: 2
+},
+{
+  question: "Which is NOT a 'recommended practice' when using JavaScript?",
+  choices: ["Ending statements with semi-colons", "closing '{' and '(', or braces and parentheses",
+    "the use of strict mode ", "Always using concat instead of + or +="],
+  answer: 3
+},
+{
+  question: "Which function adds an element at the begining of an array?",
+  choices: ["push()", "pop()", "shift()", "unshift()"],
+  answer: 3
+},
+{
+  question: "Which of the following about 'NaN' is FALSE?",
+  choices: ["NaN is numeric", "NaN === NaN", "Number.isNaN(NaN)", "NaN is a global property"],
+  answer: 1
+},
+{
+  question: "Which is TRUE about 'Math.random()' ?",
+  choices: ["It is an object", "It provides cryptographically secure random numbers", "It returns a '1' sometimes",
     "It returns a random number between 0 and 0.9999999"],
-    answer: 3
-}, {
-	question: "When creating names for Variables, which of the following is NOT allowed in JavaScript?",
-    choices: ["starting with a letter", "using letters, underscores, and dollarsigns", "using Javascript keywords",
-     "choosing case-sensitivity carefully"],
-    answer: 2
-}, {
-	question: "Which symbol meaning is WRONG in JavaScript?",
-    choices: ["++ : increment", "=== : equal value and equal type", " % : percentage", 
+  answer: 3
+},
+{
+  question: "When creating names for Variables, which of the following is NOT allowed in JavaScript?",
+  choices: ["starting with a letter", "using letters, underscores, and dollarsigns", "using Javascript keywords", "choosing case-sensitivity carefully"],
+  answer: 2
+},
+{
+  question: "Which symbol meaning is WRONG in JavaScript?",
+  choices: ["++ : increment", "=== : equal value and equal type", " % : percentage",
     "|| : logical or"],
-    answer: 2
-}, {
-	question: "Which is NOT an object?",
-    choices: ["undefined","Array", "RegExp", "a function"],
-    answer: 0
-},{
-	question: "Which is NOT a type of loop used in JavaScript?",
-    choices: ["for", "for/in", "while", "for/ a while"],
-    answer: 3
-},{
-	question: "What will the code below output to the console? console.log( 1 +  +'2' + '2' );",
-    choices: ["32", "122", "14","5"],
-    answer: 0
-}
-];
+  answer: 2
+},
+{
+  question: "Which is NOT an object?",
+  choices: ["undefined", "Array", "RegExp", "a function"],
+  answer: 0
+},
+{
+  question: "Which is NOT a type of loop used in JavaScript?",
+  choices: ["for", "for/in", "while", "for/ a while"],
+  answer: 3
+},
+{
+  question: "What will the code below output to the console? console.log( 1 +  +'2' + '2' );",
+  choices: ["32", "122", "14", "5"],
+  answer: 0
+}];
